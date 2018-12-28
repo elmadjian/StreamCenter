b419e26f7408cbdd7105722c6dc82c58392ea589
Cadu <cadu_elmadjian@yahoo.com.br>
2018-12-28 21:57:12 -0200

    adding kodi and better support for the wiimote

--- wiimote_event.py
+++ wiimote_event.py
@@ -1,21 +1,72 @@
+#!/usr/bin/python
+
 import os
+import subprocess
 import evdev
+import time
+import threading
+import signal
 from evdev import InputDevice, categorize, ecodes
 
 device = None
-devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-for d in devices:
-    if d.name == "Nintendo Wii Remote":
-        device = d
-        break
+control = threading.Condition()
+kill = False
 
-if device is not None:
-    for event in device.read_loop():
-        if event.type == ecodes.EV_KEY:
-            #button A
-            if event.code == 304 and event.value == 1:
-                os.system("xdotool click 1")
+def exit_this(signum, frame):
+    global kill
+    kill = True
 
-            #button B
-            if event.code == 305 and event.value == 1:
-                os.system("xdotool click 3")
+
+def find_device(cv, kill):
+    while not kill:
+        devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+        for d in devices:
+            if d.name == "Nintendo Wii Remote":
+                device = d
+                break
+        time.sleep(1)
+        #print(device)
+        if device is not None:
+            with cv:
+                cv.notify_all()
+                time.sleep(1)
+                cv.wait()
+
+
+def run_device(cv, kill):
+    while not kill:
+        if device is not None:
+            for event in device.read_loop():
+                if event.type == ecodes.EV_KEY:
+                    #print(event.code)
+                    
+                    #button A
+                    if event.code == 304 and event.value == 1:
+                        subprocess.Popen(["xdotool", "click", "1"])
+
+                    #button B
+                    if event.code == 305 and event.value == 1:
+                        subprocess.Popen(["xdotool", "click", "3"])
+                                
+                    #button Home
+                    if event.code == 316 and event.value == 1:
+                        os.system("killall chromium")
+                        subprocess.Popen(["chromium","--start-fullscreen",
+                                    "/home/cadu/StreamCenter/welcome.html"])
+        else:
+            with cv:
+                cv.notify_all()
+                time.sleep(1)
+                cv.wait()
+
+
+find = threading.Thread(target=find_device, args=(control, kill,))
+run  = threading.Thread(target=run_device, args=(control, kill,))
+find.start()
+run.start()
+print("joining threads...")
+find.join()
+run.join()
+print("exiting...")
+
+
